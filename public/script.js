f4058cddf8dfe08eeeae14aa6ccc20f45ac6ae74
1s64 <103432894+1s64@users.noreply.github.com>
2025-06-06 11:09:41 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', async () => {
+async function logVisit() {
   try {
     const res = await fetch('/api/log-visit', {
       method: 'POST',
@@ -8,11 +8,22 @@ window.addEventListener('load', async () => {
 
     if (res.ok) {
       console.log('Visit logged successfully.');
+      document.getElementById('clickme-button').style.display = 'none';
     } else {
       const errorData = await res.json();
       console.error('Server responded with error:', errorData.message);
+      document.getElementById('clickme-button').style.display = 'block';
     }
   } catch (err) {
     console.error('Error logging visit:', err);
+    document.getElementById('clickme-button').style.display = 'block';
   }
+}
+
+window.addEventListener('load', () => {
+  logVisit();
 });
+
+function clickmeButton() {
+  logVisit();
+}
